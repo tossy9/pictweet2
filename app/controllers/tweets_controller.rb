c6b4e7d65bb7825af9e688c590238a787c193105
江藤俊彦 <etoutoshihiko@etoutoshihikonoMacBook-puro.local>
2018-03-25 01:24:48 +0900

    ユーザページ作成 ・usersコントローラ作成 ・tweetsとusersにアソシエーション定義 ・マイページ作成 ・tweetテーブルからnickname削除 ・n+1問題対応

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,25 +3,23 @@ class TweetsController < ApplicationController
   before_action :move_to_index, except: :index
 
   def index
-    @tweets = Tweet.order('created_at DESC').page(params[:page]).per(5)
+    @tweets = Tweet.includes(:user).order('created_at DESC').page(params[:page]).per(5)
   end
 
   def new
   end
 
   def create
-    Tweet.create(tweet_params)
+    Tweet.create(image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
   end
 
   private
   def tweet_params
-    params.permit(:name, :image, :text)
+    params.permit(:image, :text)
   end
 
   def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
+    redirect_to action: :index unless user_signed_in?
   end
 
 end
