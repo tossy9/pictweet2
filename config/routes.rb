0e14245d38380613b605dc91e8f01047b4a6d50d
江藤俊彦 <etoutoshihiko@etoutoshihikonoMacBook-puro.local>
2018-03-25 15:11:29 +0900

    詳細ページを追加 ・ツイート毎にコメント可能 ・コメント一覧表示 ・他のユーザのツイートに対してもコメント可能 作業 ・commentモデル、コントローラ作成 ・アソシエーション定義 ・resourcesを用いた実装に変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   root  'tweets#index'
-  get   'tweets' => 'tweets#index'
-  get   'tweets/new' => 'tweets#new'
-  post  'tweets' => 'tweets#create'
-  delete  'tweets/:id'  => 'tweets#destroy'
-  patch   'tweets/:id'  => 'tweets#update'
-  get   'tweets/:id/edit'  => 'tweets#edit'
+  # get   'tweets' => 'tweets#index'
+  # get   'tweets/new' => 'tweets#new'
+  # post  'tweets' => 'tweets#create'
+  # delete  'tweets/:id'  => 'tweets#destroy'
+  # patch   'tweets/:id'  => 'tweets#update'
+  # get   'tweets/:id/edit'  => 'tweets#edit'
   # get   'tweets/:id'  => 'tweets#show'
-  get   'users/:id' => 'users#show'
+  resources :tweets do
+    resources :comments, only: [:create]
+  end
+  # get   'users/:id' => 'users#show'
+  resources :users, only: [:show]
 end
